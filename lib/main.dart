6a546ac2b21a2e2f8435be24ea51c1347cc8021b
SuryaSatyaNikhil503 <21a95a0503@aec.edu.in>
2023-11-11 15:41:50 +0530

    Second Commit

--- lib/main.dart
+++ lib/main.dart
@@ -25,17 +25,44 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   @override
+  String mytext = "Hello World";
+  void change() {
+    setState(() {
+      if (mytext.startsWith("H")) {
+        mytext = "Welcome to the World of Flutter";
+      } else {
+        mytext = "Hello World";
+      }
+    });
+  }
+
+  Widget _bodywidget() {
+    return Container(
+      padding: EdgeInsets.all(8.0),
+      child: new Center(
+        child: new Column(
+          children: <Widget>[
+            new Text(mytext),
+            new ElevatedButton(
+              onPressed: change,
+              child: new Text("CLICK"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: new AppBar(
-          title: new Text(
-            "My Application",
-            textAlign: TextAlign.center,
-          ),
-          centerTitle: true,
+      appBar: new AppBar(
+        title: new Text(
+          "My Application",
+          textAlign: TextAlign.center,
         ),
-        body: Center(
-          child: new Text("Hello StateFul Widget"),
-        ));
+        centerTitle: true,
+      ),
+      body: Center(child: _bodywidget()),
+    );
   }
 }
